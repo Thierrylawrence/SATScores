d4663a02a64582abeb324dac8bee14db135ddb3f
Thierrylawrence <thierry.lawrence24@trinityschoolnyc.org>
2023-05-25 13:13:04 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,23 @@
 
+from flask import Flask
+from flask import render_template
+from flask import request
+import json
+
+
+
+app = Flask(__name__, static_url_path='', static_folder='static')
+
+@app.route('/')
+def index():
+
+
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+
+
+    return render_template('about.html')
+
+app.run(debug=True)
